a96d4224f2a9dc69ffa5b35e3df077cb73a1a68a
thomasmendez <thomasmendez01@gmail.com>
2022-06-12 16:16:40 -0500

    Added styling to title header

--- src/components/TitleHeader/TitleHeaderProfile/TitleHeaderProfile.vue
+++ src/components/TitleHeader/TitleHeaderProfile/TitleHeaderProfile.vue
@@ -1,33 +1,35 @@
 <template>
-  <v-container class="grey lighten-5">
-    <v-row class="pb-5">
-      <v-col>
-        <v-row class="justify-center pb-5">
-          <v-card max-width="344" id="rounded-card">
-            <v-row>
-              <v-col>
-                <v-img
-                  height="200"
-                  width="200"
-                  id="rounded-card"
-                  src="../../../assets/pic.jpeg"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-row>
-        <v-row class="justify-center pb-5">
-          <div class="text-h4">Thomas A Mendez</div>
-        </v-row>
-        <v-row class="justify-center">
-          <div class="text-h5 text-center">
-            Software Engineer <br />
-            and Game Developer
-          </div>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div :style="{ background: this.$vuetify.theme.themes.light.background }">
+    <v-container class="grey lighten-5">
+      <v-row class="pb-5 pt-5">
+        <v-col>
+          <v-row class="justify-center pb-10">
+            <v-card max-width="344" id="rounded-card">
+              <v-row>
+                <v-col>
+                  <v-img
+                    height="200"
+                    width="200"
+                    id="rounded-card"
+                    src="../../../assets/pic.jpeg"
+                  ></v-img>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-row>
+          <v-row class="justify-center pb-3">
+            <div class="text-h4">Thomas A Mendez</div>
+          </v-row>
+          <v-row class="justify-center">
+            <div class="text-h5 text-center" id="sub-header">
+              Software Engineer <br />
+              and Game Developer
+            </div>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
@@ -41,5 +43,8 @@ export default defineComponent({
 <style scoped>
 #rounded-card {
   border-radius: 50%;
+}
+#sub-header {
+  color: #aaa;
 }
 </style>
