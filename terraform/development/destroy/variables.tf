130d0e38ede8687e893a0fdf8109f0377c3ccc3f
thomasmendez <thomasmendez01@gmail.com>
2022-07-26 00:46:39 -0500

    Added missing var

--- terraform/development/destroy/variables.tf
+++ terraform/development/destroy/variables.tf
@@ -1,3 +1,8 @@
+variable "env" {
+  description = "Environment"
+  type        = string
+  default     = "dev"
+}
 variable "aws_bucket_name" {
   description = "S3 Bucket Name"
   type        = string
