7d5e8d5fd6895aaf0196daa1412d3dc91868f970
thomasmendez <thomasmendez01@gmail.com>
2022-07-25 23:48:27 -0500

    Added option to destroy bucket

---
+++ terraform/development/destroy/bucket.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "bucketdev" {
+  bucket        = var.aws_bucket_name
+  force_destroy = true
+}
---
+++ terraform/development/destroy/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_bucket_name" {
+  description = "S3 Bucket Name"
+  type        = string
+}
