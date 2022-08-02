1b3acb63da2286f4663151dcea45df33dff920a7
thomasmendez <thomasmendez01@gmail.com>
2022-08-01 20:32:21 -0500

    Adding route 53 configuration

---
+++ terraform/development/remote/route53.tf
@@ -0,0 +1,29 @@
+resource "aws_route53_zone" "dev" {
+  name = "thomasamendez.com"
+}
+
+resource "aws_route53_zone" "dev" {
+  name = "dev.thomasamendez.com"
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_route53_record" "dev-ns" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "dev.thomasamendez.com"
+  type    = "NS"
+  ttl     = "30"
+  records = aws_route53_zone.dev.name_servers
+}
+
+resource "aws_route53_record" "route53dev" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = var.aws_bucket_name
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.lb.public_ip]
+  tags = {
+    Environment = "dev"
+  }
+}
