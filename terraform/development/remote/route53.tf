588d6debd798b9b205a05d2849f3fc5dbbe4bd9c
thomasmendez <thomasmendez01@gmail.com>
2022-08-01 21:01:59 -0500

    changed the naming of the resouce names

--- terraform/development/remote/route53.tf
+++ terraform/development/remote/route53.tf
@@ -1,29 +1,18 @@
-resource "aws_route53_zone" "test" {
+resource "aws_route53_zone" "main" {
   name = "thomasamendez.com"
 }
 
-resource "aws_route53_zone" "dev-zone" {
+resource "aws_route53_zone" "test" {
   name = "dev.thomasamendez.com"
   tags = {
     Environment = "dev"
   }
 }
 
-resource "aws_route53_record" "dev-ns" {
+resource "aws_route53_record" "test-ns" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "dev.thomasamendez.com"
   type    = "NS"
   ttl     = "30"
   records = aws_route53_zone.dev.name_servers
-}
-
-resource "aws_route53_record" "route53dev" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = var.aws_bucket_name
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
-  tags = {
-    Environment = "dev"
-  }
 }
