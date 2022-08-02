266beb2cc572db108cbb55aebe8c81e1148333da
thomasmendez <thomasmendez01@gmail.com>
2022-08-02 18:02:29 -0500

    Changed to use aws bucket name in record and zone

--- terraform/development/create/route53.tf
+++ terraform/development/create/route53.tf
@@ -1,9 +1,5 @@
-resource "aws_route53_zone" "main" {
-  name = "thomasamendez.com"
-}
-
 resource "aws_route53_zone" "test" {
-  name = "dev.thomasamendez.com"
+  name = "dev.${var.aws_bucket_name}.com"
   tags = {
     Environment = "dev"
   }
@@ -11,7 +7,7 @@ resource "aws_route53_zone" "test" {
 
 resource "aws_route53_record" "test-ns" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "dev.thomasamendez.com"
+  name    = "dev.${var.aws_bucket_name}.com"
   type    = "NS"
   ttl     = "30"
   records = aws_route53_zone.test.name_servers
