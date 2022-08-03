dc03eecc883e74171024a0f0da76039cb8e45d0c
thomasmendez <thomasmendez01@gmail.com>
2022-08-02 21:34:36 -0500

    getting correct zone id

--- terraform/development/remote/cloudfront.tf
+++ terraform/development/remote/cloudfront.tf
@@ -122,7 +122,7 @@ resource "aws_acm_certificate" "cf_gitbook" {
 }
 
 resource "aws_route53_record" "cf_gitbook_validation" {
-  zone_id = aws_route53_zone.public[0].zone_id
+  zone_id = aws_route53_zone.main[0].zone_id
   name    = aws_acm_certificate.cf_gitbook.domain_validation_options.0.resource_record_name
   type    = aws_acm_certificate.cf_gitbook.domain_validation_options.0.resource_record_type
   records = [aws_acm_certificate.cf_gitbook.domain_validation_options.0.resource_record_value]
