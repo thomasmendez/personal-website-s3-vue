b34a88ecdc1b1a00d3dbb587111bba6bfd13ea55
thomasmendez <thomasmendez01@gmail.com>
2022-08-09 00:22:11 -0500

    Format

--- terraform/development/remote/route53.tf
+++ terraform/development/remote/route53.tf
@@ -4,12 +4,12 @@ resource "aws_route53_zone" "main" {
 
 resource "aws_route53_record" "cert_validation" {
   zone_id = aws_route53_zone.main.zone_id
-  name = "thomasamendez.com"
-  type = "A"
+  name    = "thomasamendez.com"
+  type    = "A"
 
   alias {
-    name = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
