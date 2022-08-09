09077a33aa8f328cab0173f1171c19e78286715a
thomasmendez <thomasmendez01@gmail.com>
2022-08-08 23:50:09 -0500

    Adding records again

--- terraform/development/remote/route53.tf
+++ terraform/development/remote/route53.tf
@@ -1,3 +1,17 @@
 resource "aws_route53_zone" "main" {
   name = "thomasamendez.com"
 }
+resource "aws_route53_zone" "test" {
+  name = "dev.thomasamendez.com"
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_route53_record" "test-ns" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "dev.thomasamendez.com"
+  type    = "NS"
+  ttl     = "30"
+  records = aws_route53_zone.test.name_servers
+}
