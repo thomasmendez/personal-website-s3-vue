0d50b482da95e34d7dbfa78e90da256f036c19d6
thomasmendez <thomasmendez01@gmail.com>
2022-08-11 00:56:00 -0500

    Remove unessesary stuff

--- terraform/development/remote/cloudfront.tf
+++ terraform/development/remote/cloudfront.tf
@@ -1,8 +1,11 @@
 locals {
-  s3_origin_id = "myS3Origin"
+  s3_origin_id = aws_s3_bucket.bucketdev.id
 }
 resource "aws_cloudfront_origin_access_identity" "bucketdev" {
   comment = "bucketdev"
+}
+data "aws_cloudfront_cache_policy" "cache_policy" {
+    name = "Managed-CachingOptimized"
 }
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
@@ -23,74 +26,25 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
+    # cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
 
-    forwarded_values {
-      query_string = false
+    # forwarded_values {
+    #   query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
+    #   cookies {
+    #     forward = "none"
+    #   }
+    # }
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
     viewer_protocol_policy = "redirect-to-https"
+    cache_policy_id = data.aws_cloudfront_cache_policy.cache_policy.id
   }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  price_class = "PriceClass_200"
 
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
+      locations        = ["US", "CA"]
     }
   }
 
@@ -104,9 +58,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   #   minimum_protocol_version = "TLSv1.2_2018"
   # }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+  # viewer_certificate {
+  #   cloudfront_default_certificate = true
+  # }
 }
 
 # resource "aws_acm_certificate" "cert" {
