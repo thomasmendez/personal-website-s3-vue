1cfb98998cf8aae3da31926b35676d529df145cc
thomasmendez <thomasmendez01@gmail.com>
2022-08-15 00:19:07 -0500

    Format and set protocol version

--- terraform/development/remote/cloudfront.tf
+++ terraform/development/remote/cloudfront.tf
@@ -62,8 +62,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   # }
 
   viewer_certificate {
-    acm_certificate_arn = data.aws_acm_certificate.amazon_cert.arn
-    ssl_support_method  = "sni-only"
+    acm_certificate_arn      = data.aws_acm_certificate.amazon_cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
 }
