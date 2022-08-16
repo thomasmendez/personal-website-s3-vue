f663781b409e7a58c0fdf824bcfe53b9b1f03222
thomasmendez <thomasmendez01@gmail.com>
2022-08-16 01:06:02 -0500

    Removed development tf files, corrected sub domain storybook issue

--- terraform/development/remote/variables.tf
+++ terraform/development/remote/variables.tf
@@ -7,9 +7,3 @@ variable "aws_bucket_name" {
   description = "S3 Bucket Name"
   type        = string
 }
-
-variable "domain" {
-  description = "Domain Name"
-  type        = string
-  default     = "thomasamendez.com"
-}
