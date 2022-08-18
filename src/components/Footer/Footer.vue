377977ddea1410713d6a6c20e5dd4090c10d3c6b
Thomas Mendez <thomasmendez01@gmail.com>
2022-08-18 18:12:12 -0500

    Initial footer commit

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-col>
+      <v-row> © {{ currentYear }} Copyright </v-row>
+    </v-col>
+    <v-col>
+      <v-row> Contact Methods </v-row>
+    </v-col>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Footer",
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+});
+</script>
+
+<style scoped></style>
