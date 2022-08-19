2e6e0e73ee877d005d1c171b19941933fc822954
thomasmendez <thomasmendez01@gmail.com>
2022-08-18 23:50:49 -0500

    Added Footer, removed cloudfront.tf from destroy production module, removed unessesary lines from AboutMe.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,10 +1,29 @@
 <template>
   <v-container>
-    <v-col>
-      <v-row> © {{ currentYear }} Copyright </v-row>
-    </v-col>
-    <v-col>
-      <v-row> Contact Methods </v-row>
+    <v-col class="d-flex flex-row">
+      <v-row>
+        <v-col xs="12" sm="9" md="9" lg="10" xl="10">
+          <div>© {{ currentYear }} Copyright</div>
+        </v-col>
+        <v-col class="flex-row-reverse" xs="12" sm="3" md="3" lg="2" xl="2">
+          <v-btn flat icon>
+            <v-icon>mdi-linkedin</v-icon>
+            <span class="iconify" data-icon="mdi:linkedin"></span>
+          </v-btn>
+          <a href="https://www.linkedin.com/in/thomas-a-mendez" target="_blank">
+          </a>
+          <v-btn flat icon>
+            <v-icon>mdi-github</v-icon>
+            <span class="iconify" data-icon="mdi:github"></span>
+          </v-btn>
+          <a href="https://github.com/thomasmendez" target="_blank"></a>
+          <v-btn flat icon>
+            <v-icon>mdi-email</v-icon>
+            <span class="iconify" data-icon="mdi:email"></span>
+          </v-btn>
+          <a href="mailto:thomasmendez01@gmail.com" target="_blank"></a>
+        </v-col>
+      </v-row>
     </v-col>
   </v-container>
 </template>
@@ -22,4 +41,15 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+a {
+  text-decoration: none;
+  color: inherit;
+}
+a:hover {
+  color: inherit;
+}
+.v-container {
+  border-top: solid 1px gray;
+}
+</style>
